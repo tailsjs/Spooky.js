243c789bd0431938de56dc5d9559b7621c3a2c67
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-02-23 18:38:12 +0400

    [v1.0] - FastFix of Crashes

--- Logic/Player.js
+++ Logic/Player.js
@@ -1,6 +1,6 @@
 class Player {
     lowID = 0
-    token = ""
+    token = "ISwearThisTokenIsGood"
     name = "Spooky.js"
     cards = [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 95, 100, 105, 110, 115, 120, 125, 130, 177, 182, 194]
     homeBrawler = 0
